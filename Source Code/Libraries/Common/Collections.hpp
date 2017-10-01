25687f73bf3ffbd2365742ef0fbb942abab40994
TheGreatV <GreatV@email.ua>
2017-10-01 16:44:05 +0300

    [Update] Vector plus operator

--- Source Code/Libraries/Common/Collections.hpp
+++ Source Code/Libraries/Common/Collections.hpp
@@ -23,10 +23,40 @@ namespace GreatVulkan
 	template<class Key_, class Value_>	using Map			= std::map<Key_, Value_>;
 	template<class Type_>				using Set			= std::set<Type_>;
 	template<class Type_>				using Initializer	= std::initializer_list<Type_>;
+
+
+	template <typename T>
+	Vector<T> operator + (const Vector<T>& a_, const Vector<T>& b_);
+	template <typename T>
+	Vector<T> operator + (const Vector<T>& a_, const T& b_);
 }
 
 
 #pragma region GreatVulkan
+
+template <typename T>
+GreatVulkan::Vector<T> GreatVulkan::operator + (const Vector<T>& a_, const Vector<T>& b_)
+{
+	Vector<T> ab;
+
+	ab.reserve(a_.size() + b_.size());
+	ab.insert(ab.end(), a_.begin(), a_.end());
+	ab.insert(ab.end(), b_.begin(), b_.end());
+
+	return Move(ab);
+}
+template <typename T>
+GreatVulkan::Vector<T> GreatVulkan::operator + (const Vector<T>& a_, const T& b_)
+{
+	Vector<T> ab;
+
+	ab.reserve(a_.size() + 1);
+	ab.insert(ab.end(), a_.begin(), a_.end());
+	ab.push_back(b_);
+
+	return Move(ab);
+}
+
 #pragma endregion
 
 
